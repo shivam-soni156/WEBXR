a9fb10fe66217d001e8fc8d0b886319941ba6e58
Shivam Soni <78346328+shivam-soni156@users.noreply.github.com>
2022-08-30 11:40:07 +0530

    Add files via upload

---
+++ disappear.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('disappear', {
+  
+    init: function () {
+    
+      let disappearfunc= () => {
+        this.el.setAttribute("visible", false);
+      };
+
+      this.el.addEventListener('click',disappearfunc);
+
+    }
+    
+   });
+    
+  
---
+++ examplefunctions.js
@@ -0,0 +1,8 @@
+// Simple example function: 
+let addingfunction = (x, y) => {
+    return x + y;   // The function returns the product of x and y
+  }
+  
+  console.log(addingfunction(3, 5)); //console will log 8
+  
+  
